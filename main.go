7e1316f449ff5491d5ceee5f1509f8055a492de0
robogg133 <Luizfernandojoaquimdasilva@proton.me>
2026-08-08 11:12:33 -0300

    feat: new list pods route and robots.txt

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"embed"
+	"io"
 	"net/http"
 
 	"git.servidordomal.lol/robogg133/superultraomegadeploy/internal/shared/response"
@@ -9,6 +11,8 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/rs/zerolog/log"
 )
+
+var includeFs embed.FS
 
 func main() {
 	initLogger()
@@ -28,15 +32,41 @@ func main() {
 	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 	})
-
-	r.Get("/api/v1/pods/list", func(w http.ResponseWriter, r *http.Request) {
-		_, err := k.ListPods(r.Context())
+	r.Get("/robots.txt", func(w http.ResponseWriter, r *http.Request) {
+		f, err := includeFs.Open("include/robots.txt")
 		if err != nil {
-			log.Err(err).Send()
+			log.Ctx(r.Context()).Err(err).Send()
 			response.New().InternalServerError(w)
 			return
 		}
-		response.New().Send(w, 200)
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(200)
+		if _, err := io.Copy(w, f); err != nil {
+			log.Ctx(r.Context()).Err(err).Send()
+			response.New().InternalServerError(w)
+			return
+		}
+	})
+
+	r.Get("/api/v1/pods/list", func(w http.ResponseWriter, r *http.Request) {
+		list, err := k.ListPods(r.Context())
+		if err != nil {
+			log.Ctx(r.Context()).Err(err).Send()
+			response.New().InternalServerError(w)
+			return
+		}
+		resp := make([]map[string]any, 0)
+
+		for _, pod := range list {
+			r := make(map[string]any)
+			r["name"] = pod.Name
+			r["namespace"] = pod.Namespace
+			r["creation_time"] = pod.CreationTimestamp.Time
+			resp = append(resp, r)
+		}
+		log.Ctx(r.Context()).Debug().Int("len_pods", len(resp)).Send()
+
+		response.New().Response(resp).Send(w, 200)
 	})
 
 	hserver := http.Server{
